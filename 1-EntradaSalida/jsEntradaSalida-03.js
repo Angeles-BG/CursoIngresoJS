e95b3566b6a2c0deb0a7da3384182b46e21609e5
Angeles-BG <g.angeles112004@gmail.com>
2023-02-01 09:27:56 -0300

    modifique el ejercicio 3.

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -2,16 +2,25 @@
 García Ángeles Belén. 
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
+function mostrar() {
+	
 	let nombre;
-	/* otra forma que puede o no funcionar: 
+	let mensaje;
+
+	/*
+	--------------------------
+	otra forma que puede o no funcionar: 
 	nombre = txtIdNombre.value;
+	--------------------------
+	document --> llama al html 
+	getElementById --> Busca en el html por el ID, el ID que esta ntre parentesis.
+	value --> trae el valor que este dentro de ese elemento. 
+	--------------------------
 	*/
 	nombre = document.getElementById("txtIdNombre").value;
-	alert (nombre);
-	document.getElementById("txtIdNombre").value = "";
-	//limpio la caja de texto.
+	mensaje = "Usted ingreso el nombre: " + nombre;
+	alert(mensaje);
+
 }
 
 

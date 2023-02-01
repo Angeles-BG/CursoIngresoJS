ccc89f52721bd215a579ccfdc78a2dd0f3c546ef
Angeles-BG <g.angeles112004@gmail.com>
2023-02-01 13:57:35 -0300

    modificado tp2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,4 +1,6 @@
-/*2.	Para el departamento de Construcción:
+/*
+García Ángeles Belén. 
+2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
@@ -10,6 +12,7 @@ function Rectangulo ()
     let ancho;
     let perimetro;
     let cantalambre;
+    let mensaje; 
 
     largo = document.getElementById("txtIdLargo").value;
     ancho = document.getElementById("txtIdAncho").value;
@@ -19,8 +22,9 @@ function Rectangulo ()
 
     perimetro = (largo + ancho ) *2 ;
     cantalambre = perimetro *3;
+    mensaje = "La cantidad de alambre a comprar es de " + cantalambre + " metros";
 
-    alert("La cantidad de alambre a comprar es de " + cantalambre + " metros");
+    alert(mensaje);
 
 }
 function Circulo () 
@@ -28,16 +32,18 @@ function Circulo ()
 	let radio;
     let perimetro;
     let cantalambre;
-    const PI = 3.14;
+    let pi = 3.14;
+    let mensaje; 
 
     radio = document.getElementById("txtIdRadio").value;
     
     radio = parseFloat(radio);
    
-    perimetro = 2*PI*radio ;
+    perimetro = 2*pi*radio ;
     cantalambre = perimetro *3;
+    mensaje = "La cantidad de alambre a comprar es de " + cantalambre + " metros"
 
-    alert("La cantidad de alambre a comprar es de " + cantalambre + " metros");
+    alert(mensaje);
 }
 function Materiales () 
 {
@@ -46,6 +52,7 @@ function Materiales ()
     let area;
     let cemento ;
     let cal;
+    let mensaje; 
 
     largo = document.getElementById("txtIdLargo").value;
     ancho = document.getElementById("txtIdAncho").value;
@@ -56,8 +63,9 @@ function Materiales ()
     area = (ancho*largo);
     cemento = (area*2);
     cal = (area*3);
+    mensaje = "Para un contrapiso de "  + area + " m2 necesito comprar " + cemento + " bolsas de cemento y " + cal + " bolsas de cal."
 
-    alert("Para un contrapiso de "  + area + " m2 necesito comprar " + cemento + " bolsas de cemento y " + cal + " bolsas de cal.");
+    alert(mensaje);
 
 
     

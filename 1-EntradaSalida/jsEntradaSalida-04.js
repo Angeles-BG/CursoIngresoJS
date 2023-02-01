0d42d85561a9744721e5bd89e10d0768b23c0beb
Angeles-BG <g.angeles112004@gmail.com>
2023-02-01 09:48:39 -0300

    modifique el ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,11 +1,14 @@
-/* 
+/*
+García Ángeles Belén. 
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
 	let nombre; // declaro una variable. 
-	nombre = prompt("Ingrese su nombre"); //tomo un dato ingresado por el usuario y lo guardo en la variable nombre. 
-	document.getElementById("txtIdNombre").value = nombre; //coloco en la caja de texto envevevida en el html lo guardado en la variable nombre.
+	nombre = prompt("Ingrese su nombre"); 
+	//tomo un dato ingresado por el usuario y lo guardo en la variable nombre. 
+	document.getElementById("txtIdNombre").value = nombre; 
+	//coloco en la caja de texto que esta en el html, y lo guardado en la variable nombre.
 }
 

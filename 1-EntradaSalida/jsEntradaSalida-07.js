10e7c61ee1eac6bebad3df3dc3582ab252074c81
Angeles-BG <g.angeles112004@gmail.com>
2023-02-01 11:33:49 -0300

    modifique el ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,4 +1,5 @@
 /*
+García Ángeles Belen. 
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
@@ -6,18 +7,18 @@ ej.: "la Resta es 750"*/
 function sumar()
 {	
 	//declaro variables.
-	let num1;
-	let num2;
+	let numUno;
+	let numDos;
 	let total;
 
 	//guardo, lo ingresado por el usuario en las caja de texto, en las variables correspondientes. 
-	num1 = document.getElementById("txtIdNumeroUno").value;
-	num2 = document.getElementById("txtIdNumeroDos").value;
+	numUno = document.getElementById("txtIdNumeroUno").value;
+	numDos = document.getElementById("txtIdNumeroDos").value;
     
 	//transformo lo ingresado en la caja de texto a numero entero, sumo los numeros y los almaceno en una nueva variable.  
-	num1 = parseInt(num1);
-	num2 = parseInt(num2);
-	total = num1 + num2;
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	total = numUno + numDos;
 
 	//muestro mediante un alert la suma.
 	alert("La suma es  " + total );
@@ -27,18 +28,18 @@ function sumar()
 function restar()
 {
 	//declaro variables.
-	let num1;
-	let num2;
+	let numUno;
+	let numDos;
 	let total;
 
 	//guardo, lo ingresado por el usuario en las caja de texto, en las variables correspondientes. 
-	num1 = document.getElementById("txtIdNumeroUno").value;
-	num2 = document.getElementById("txtIdNumeroDos").value;
+	numUno = document.getElementById("txtIdNumeroUno").value;
+	numDos = document.getElementById("txtIdNumeroDos").value;
     
 	//transformo lo ingresado en la caja de texto a numero entero, resto los numeros y los almaceno en una nueva variable.  
-	num1 = parseInt(num1);
-	num2 = parseInt(num2);
-	total = num1 - num2;
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	total = numUno - numDos;
 
 	//muestro mediante un alert la resta.
 	alert("La resta es  " + total );
@@ -48,18 +49,18 @@ function restar()
 function multiplicar()
 { 
 	//declaro variables.
-	let num1;
-	let num2;
+	let numUno;
+	let numDos;
 	let total;
 
 	//guardo, lo ingresado por el usuario en las caja de texto, en las variables correspondientes. 
-	num1 = document.getElementById("txtIdNumeroUno").value;
-	num2 = document.getElementById("txtIdNumeroDos").value;
+	numUno = document.getElementById("txtIdNumeroUno").value;
+	numDos = document.getElementById("txtIdNumeroDos").value;
     
 	//transformo lo ingresado en la caja de texto a numero entero, multiplico los numeros y los almaceno en una nueva variable.  
-	num1 = parseInt(num1);
-	num2 = parseInt(num2);
-	total = num1 * num2;
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	total = numUno * numDos;
 
 	//muestro mediante un alert la multiplicación.
 	alert("La multiplicación es  " + total );
@@ -69,18 +70,18 @@ function multiplicar()
 function dividir()
 {
 	//declaro variables.
-	let num1;
-	let num2;
+	let numUno;
+	let numDos;
 	let total;
 
 	//guardo, lo ingresado por el usuario en las caja de texto, en las variables correspondientes. 
-	num1 = document.getElementById("txtIdNumeroUno").value;
-	num2 = document.getElementById("txtIdNumeroDos").value;
+	numUno = document.getElementById("txtIdNumeroUno").value;
+	numDos = document.getElementById("txtIdNumeroDos").value;
     
 	//transformo lo ingresado en la caja de texto a numero entero, divido los numeros y los almaceno en una nueva variable.  
-	num1 = parseInt(num1);
-	num2 = parseInt(num2);
-	total = num1 / num2;
+	numUno = parseInt(numUno);
+	numDos = parseInt(numDos);
+	total = numUno / numDos;
 
 	//muestro mediante un alert la divición.
 	alert("La divicion es  " + total );

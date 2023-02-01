747f3fb64290b37dc1abff2d283ff9ae5c06aa72
Angeles-BG <g.angeles112004@gmail.com>
2023-02-01 09:51:30 -0300

    modifique el ejercicio 5

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,4 +1,5 @@
 /*
+García Ángeles Belén.
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
@@ -6,12 +7,12 @@ function mostrar()
 
 	let nombre;
 	let edad;
-	nombre = document.getElementById("txtIdNombre").value; //le asigno a la variable nombre el valor introducido por el usuario en la caja de texto nombre. 
-	edad = document.getElementById("txtIdEdad").value; //le asigno a la variable edad el valor introducido por el usuario en la caja de texto edad. 
-	alert("Usted se llama " + nombre + " y tiene " + edad +" años."); //muestro lo almacenado en las variables con un texto concandenado, mediante la utilizacion de un alert. 
-	document.getElementById("txtIdNombre").value = " ";//vacio las caja de texto.
-	document.getElementById("txtIdEdad").value = " ";  //vacio las cajas de texto. 
-	//alert(`Usted se llama $(nombre) y tiene $(edad) años`);plantilla general. tambien se puede utilizar como el alert anterior. 
+	nombre = document.getElementById("txtIdNombre").value; 
+	//le asigno a la variable nombre el valor introducido por el usuario en la caja de texto nombre. 
+	edad = document.getElementById("txtIdEdad").value; 
+	//le asigno a la variable edad el valor introducido por el usuario en la caja de texto edad. 
+	alert("Usted se llama " + nombre + " y tiene " + edad +" años."); 
+	//muestro lo almacenado en las variables con un texto concandenado, mediante la utilizacion de un alert. 
 
 
 }

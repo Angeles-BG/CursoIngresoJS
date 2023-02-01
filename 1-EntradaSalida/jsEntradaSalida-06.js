1a21e943bdc656111a48377703553658f4be83ce
Angeles-BG <g.angeles112004@gmail.com>
2023-02-01 11:00:05 -0300

    modifique ejercicio 6

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,26 +1,30 @@
 /*
-García Ángeles Belén
+García Ángeles Belén.
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
 	//declaro variables.
-	let num1;
-	let num2;
+	let numUno;
+	let numDos;
+	let numUnoTexto;
+	let numDosTexto;
 	let total;
+	let mensaje; 
 
 	//guardo, lo ingresado por el usuario en las caja de texto, en las variables correspondientes. 
-	num1 = document.getElementById("txtIdNumeroUno").value;
-	num2 = document.getElementById("txtIdNumeroDos").value;
+	numUnoTexto = document.getElementById("txtIdNumeroUno").value;
+	numDosTexto = document.getElementById("txtIdNumeroDos").value;
     
 	//transformo lo ingresado en la caja de texto a numero entero, sumo los numeros y los almaceno en una nueva variable.  
-	num1 = parseInt(num1);
-	num2 = parseInt(num2);
-	total = num1 + num2;
+	numUno = parseInt(numUnoTexto);
+	numDos = parseInt(numDosTexto);
+	total = numUno + numDos;
+	mensaje = "La suma es  " + total; 
 
 	//muestro mediante un alert la suma.
-	alert("La suma es  " + total );
+	alert(mensaje);
 
 }
 

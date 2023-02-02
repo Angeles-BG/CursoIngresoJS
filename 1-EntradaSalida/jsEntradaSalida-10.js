663273aa0a97e8bda8dfe2f4511c13807374b183
Angeles-BG <g.angeles112004@gmail.com>
2023-02-02 09:25:14 -0300

    modificación del ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -10,8 +10,10 @@ function mostrarAumento()
 {
 	//declaro variables.
 	let sueldo;
-	let descuento;
+	let porcentaje;
 	let nuevosueldo;
+	let porcentajeModificar; 
+	porcentajeModificar = -25/100; 
 
 	//la suma del sueldo mas el aumento da lugar a nuestro nuevo sueldo.
 	//el aumento va ser igual a multiplicar el porcentaje por el sueldo, dividido por 100.
@@ -20,15 +22,14 @@ function mostrarAumento()
 	sueldo = document.getElementById("txtIdImporte").value;
 	//Convierto el dato obtenido en un dato del tipo numerico flotante, es decir un numero entero con coma. 
 	sueldo = parseFloat(sueldo);
-	//calculo el descuento y lo almaceno en la variable descuento.
-	descuento = sueldo* 0.25;
+	//calculo el descuento y lo almaceno en la variable porcentaje.
+	porcentaje = sueldo * porcentajeModificar;
 	//Calculo el nuevo sueldo. 
-	nuevosueldo = sueldo - descuento;
+	nuevosueldo = sueldo + porcentaje;
     //muestro en la caja de texto el resultado obtenido y almacenado en la variable nuevosueldo. 
 	document.getElementById("txtIdResultado").value = nuevosueldo;
 }
 */
-
 /*
 Ejercicio 10.bis: Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego

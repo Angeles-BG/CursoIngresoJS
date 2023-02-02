4026029414655f53b9267ad1686c8e379ce6d58f
Angeles-BG <g.angeles112004@gmail.com>
2023-02-02 12:13:17 -0300

    modificación del tp2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -8,19 +8,22 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    //A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
     let largo;
     let ancho;
     let perimetro;
     let cantalambre;
     let mensaje; 
 
+    //traigo el contenido de las cajas de texto del html. 
     largo = document.getElementById("txtIdLargo").value;
     ancho = document.getElementById("txtIdAncho").value;
-
+    //parseo lo que tengo dentro de las variables largo y ancho. Convierto en numero lo que traje del html. 
     largo = parseFloat(largo);
     ancho = parseFloat(ancho);
-
+    //calculo el perimetro del rectangulo. 
     perimetro = (largo + ancho ) *2 ;
+    //como son tres tira de alambre las que me pide, multiplico el promedio por tres. 
     cantalambre = perimetro *3;
     mensaje = "La cantidad de alambre a comprar es de " + cantalambre + " metros";
 
@@ -29,6 +32,7 @@ function Rectangulo ()
 }
 function Circulo () 
 {
+    //B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 	let radio;
     let perimetro;
     let cantalambre;
@@ -38,7 +42,7 @@ function Circulo ()
     radio = document.getElementById("txtIdRadio").value;
     
     radio = parseFloat(radio);
-   
+   //calculo el perimetro de un circulo. 
     perimetro = 2*pi*radio ;
     cantalambre = perimetro *3;
     mensaje = "La cantidad de alambre a comprar es de " + cantalambre + " metros"
@@ -47,6 +51,7 @@ function Circulo ()
 }
 function Materiales () 
 {
+    //C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 	let largo;
     let ancho;
     let area;
@@ -59,8 +64,9 @@ function Materiales ()
 
     largo = parseFloat(largo);
     ancho = parseFloat(ancho);
-
+    //Calculo el area del rectangulo que me da ya en metros cuadrados. 
     area = (ancho*largo);
+    //multiplico el area por las cantidades de material que me pide el ejercicio. 
     cemento = (area*2);
     cal = (area*3);
     mensaje = "Para un contrapiso de "  + area + " m2 necesito comprar " + cemento + " bolsas de cemento y " + cal + " bolsas de cal."

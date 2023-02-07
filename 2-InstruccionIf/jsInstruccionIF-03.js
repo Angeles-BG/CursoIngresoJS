786a508328a727e93e79c3d302de6c357ef78f8c
Angeles-BG <g.angeles112004@gmail.com>
2023-02-07 09:48:46 -0300

    modifique ejercicio tresIf

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,11 +1,14 @@
 function mostrar()
 {
+	//declaro variables
 	let edad;
+	//tomo el dato y lo parseo. 
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 
+	//escribo la condición a cumplir. 
 	if( edad >= 18){
 		alert("La persona es mayor de edad. ");
-	}else{
+	}else{ //escribo lo que pasa si la condición no se cumple. 
 		alert("La persona es menor de edad. ");
 	}
 	

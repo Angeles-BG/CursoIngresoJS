949cc48cf4aecfc61dc8c7e1aa718b3d4e496764
Angeles-BG <g.angeles112004@gmail.com>
2023-02-07 11:44:27 -0300

    modifique ejercicios If

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -3,15 +3,17 @@ function mostrar()
 {
 	//declaro variables
 	let edad;
+	let mensaje; 
 	//tomo el dato y lo parseo. 
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 
 	//escribo la condición a cumplir. 
 	if( edad >= 18){
-		alert("La persona es mayor de edad. ");
+	  mensaje = "La persona es mayor de edad. ";
 	}else{ //escribo lo que pasa si la condición no se cumple. 
-		alert("La persona es menor de edad. ");
+	    mensaje = "La persona es menor de edad. ";
 	}
+	alert(mensaje); 
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,18 +1,17 @@
-function mostrar()
-{
+function mostrar() {
 	let edad;
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 
-	if( edad >= 18){
+	if (edad >= 18) {
 		alert("La persona es mayor de edad");
 	}
-	else if (edad>=13 && edad <=17) {
-			alert("La persona es adolecente");
-		} 
-	else {
-	alert("La persona es un niño");
+	else if (edad >= 13 && edad <= 17) {
+		alert("La persona es adolecente");
 	}
- 
-	
-	
+	else {
+		alert("La persona es un niño");
+	}
+
+
+
 }//FIN DE LA FUNCIÓN

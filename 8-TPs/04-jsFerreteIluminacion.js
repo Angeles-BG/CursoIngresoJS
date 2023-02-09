5ec17c6b8151ae935bbd951e28be3dc9578ae5d7
Angeles-BG <g.angeles112004@gmail.com>
2023-02-09 12:10:20 -0300

    modificación de tpCuatro

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -9,88 +9,88 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 
  */
 
- //Ángeles Belén García 
- /*
+//Ángeles Belén García 
+/*
 function CalcularPrecio() {
-    let precio= 35;
-    let cantidad;
-    let marca;
-    let descuento = 0;
-    let importefinal;
-    let iibb;
-    let precioConDescuento;
-    let mensaje; 
+   let precio= 35;
+   let cantidad;
+   let marca;
+   let descuento = 0;
+   let importefinal;
+   let iibb;
+   let precioConDescuento;
+   let mensaje; 
 
-    cantidad = parseInt(document.getElementById("txtIdCantidad").value);
-    marca = document.getElementById("Marca").value;
+   cantidad = parseInt(document.getElementById("txtIdCantidad").value);
+   marca = document.getElementById("Marca").value;
 
 
-    if (cantidad < 3) {
-        descuento = 0;
-    } else if (cantidad == 3 ) {
-        if (marca == "ArgentinaLuz") {
-            descuento = 15;
-        } else if (marca == "FelipeLamparas") {
-            descuento = 10;
-        } else {
-            descuento = 5;
-        }
-    } else if (cantidad == 4) {
-        if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
-            descuento = 25;
-        } else {
-            descuento = 20;
-        }
-    } else if (cantidad == 5) {
-        if (marca == "ArgentinaLuz") {
-            descuento = 40;
-        } else {
-            descuento = 30;
-        }
-    } else {
-        descuento = 50;
-    }
+   if (cantidad < 3) {
+       descuento = 0;
+   } else if (cantidad == 3 ) {
+       if (marca == "ArgentinaLuz") {
+           descuento = 15;
+       } else if (marca == "FelipeLamparas") {
+           descuento = 10;
+       } else {
+           descuento = 5;
+       }
+   } else if (cantidad == 4) {
+       if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+           descuento = 25;
+       } else {
+           descuento = 20;
+       }
+   } else if (cantidad == 5) {
+       if (marca == "ArgentinaLuz") {
+           descuento = 40;
+       } else {
+           descuento = 30;
+       }
+   } else {
+       descuento = 50;
+   }
 // ejemplo con 5 lamparitas de marca OSRAM 
-    precioConDescuento = precio - precio * descuento / 100;
-    //      24,5             35 -  35     *  30     / 1000
-    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
-    importefinal = precioConDescuento * cantidad;
-    //     122,5               24,5           * 5
+   precioConDescuento = precio - precio * descuento / 100;
+   //      24,5             35 -  35     *  30     / 1000
+   document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
+   importefinal = precioConDescuento * cantidad;
+   //     122,5               24,5           * 5
 
-    if (importefinal > 120) {
-        iibb = importefinal * 10 / 100;
-        //12,25     122,5   *10 /  100
-        importefinal += iibb;
-        // 134,75            122,5 + 12,25
-        mensaje = "Total: $ " + importefinal + ". Usted pago $ " + iibb + " de IIBB.";
-        //                         134,75                          12,25
-    } else {
-        mensaje = "Total: $ " + importefinal;
-                                
-    }
-    alert(mensaje); 
+   if (importefinal > 120) {
+       iibb = importefinal * 10 / 100;
+       //12,25     122,5   *10 /  100
+       importefinal += iibb;
+       // 134,75            122,5 + 12,25
+       mensaje = "Total: $ " + importefinal + ". Usted pago $ " + iibb + " de IIBB.";
+       //                         134,75                          12,25
+   } else {
+       mensaje = "Total: $ " + importefinal;
+                               
+   }
+   alert(mensaje); 
 }
 */
 function CalcularPrecio() {
-    let precio= 35;
+    let precio = 35;
     let cantidad;
     let marca;
     let descuento = 0;
     let importefinal;
     let iibb;
     let precioConDescuento;
-    let mensaje; 
+    let mensaje;
 
     cantidad = parseInt(document.getElementById("txtIdCantidad").value);
     marca = document.getElementById("Marca").value;
 
-    switch(cantidad){
+    switch (cantidad) {
         case 1:
-        case 2: 
-        descuento = 0;
-        break;
+        case 2:
+            descuento = 0;
+            break;
         case 3:
-            switch(marca){
+            switch (marca) {
                 case "ArgentinaLuz":
                     descuento = 15;
                     break;
@@ -99,52 +99,45 @@ function CalcularPrecio() {
                     break;
                 default:
                     descuento = 5;
-                break; 
+                    break;
             }
             break;
         case 4:
-            switch(marca){
+            switch (marca) {
                 case "ArgentinaLuz":
                 case "FelipeLamparas":
                     descuento = 25;
-                break;
+                    break;
                 default:
                     descuento = 20;
-                break; 
+                    break;
             }
             break;
-        case 5: 
-            switch(marca){
+        case 5:
+            switch (marca) {
                 case "ArgentinaLuz":
                     descuento = 40;
-                break;
+                    break;
                 default:
                     descuento = 30;
-                break;
+                    break;
             }
             break;
         default:
             descuento = 50;
-        break; 
+            break;
     }
 
-    // ejemplo con 5 lamparitas de marca OSRAM 
     precioConDescuento = precio - precio * descuento / 100;
-    //      24,5             35 -  35     *  30     / 1000
     document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
     importefinal = precioConDescuento * cantidad;
-    //     122,5               24,5           * 5
 
     if (importefinal > 120) {
         iibb = importefinal * 10 / 100;
-        //12,25     122,5   *10 /  100
         importefinal += iibb;
-        // 134,75            122,5 + 12,25
         mensaje = "Total: $ " + importefinal + ". Usted pago $ " + iibb + " de IIBB.";
-        //                         134,75                          12,25
     } else {
         mensaje = "Total: $ " + importefinal;
-                                
     }
-    alert(mensaje); 
+    alert(mensaje);
 }

0e5c28076b88dfc69ed6419fe886f2d45e0581b6
Angeles-BG <g.angeles112004@gmail.com>
2023-02-09 11:54:52 -0300

    modificación de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,33 +1,32 @@
 //Angeles Belen Garcia
-function mostrar()
-{
-	let mes; 
-	let mensaje; 
+function mostrar() {
+	let mes;
+	let mensaje;
 	mes = document.getElementById("txtIdMes").value;
-	
-switch(mes){
-	case "Febrero": 
-		mensaje = " Este mes no tiene 28 o 29 días.";
-	break;
-	case "Enero": 
-    case "Abril": 
-    case "Junio": 
-    case "Agosto": 
-    case "Octubre": 
-    case "Diciembre": 
-		mensaje = "Este mes tiene 31 días";
-	break;
-	case "Marzo": 
-	case "Mayo": 
-	case "Julio": 
-	case "Septiembre": 
-	case "Noviembre": 
-		mensaje = "Este mes tiene 30 días";
-	break;
-//cuando el programa no entra a ningun case por defecto entrara a este. 
-	default: 
-	mensaje = "debe seleccionar un mes valido";
-	break; 
-} 
+
+	switch (mes) {
+		case "Febrero":
+			mensaje = " Este mes no tiene 28 o 29 días.";
+			break;
+		case "Enero":
+		case "Abril":
+		case "Junio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			mensaje = "Este mes tiene 31 días";
+			break;
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Septiembre":
+		case "Noviembre":
+			mensaje = "Este mes tiene 30 días";
+			break;
+		//cuando el programa no entra a ningun case por defecto entrara a este. 
+		default:
+			mensaje = "debe seleccionar un mes valido";
+			break;
+	}
 	alert(mensaje);
 }//FIN DE LA FUNCIÓN

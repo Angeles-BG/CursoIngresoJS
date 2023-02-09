dedb3410528a0b179775c6b426c82765cd842576
Angeles-BG <g.angeles112004@gmail.com>
2023-02-09 11:58:07 -0300

    modificación de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,16 +1,22 @@
-function mostrar()
-{
-	//tomo la hora
-	let horaDelDia =parseInt(document.getElementById("txtIdHora").value);
-	
-	switch (horaDelDia){
-		case 7: 
+//Angles Belen Garcia
+function mostrar() {
+
+	let mensaje;
+	let horaDelDia;
+	horaDelDia = parseInt(document.getElementById("txtIdHora").value);
+
+	switch (horaDelDia) {
+		case 7:
 		case 8:
 		case 9:
 		case 10:
-		case 11: 
-		alert( "Es de mañana.");
-		break; 
+		case 11:
+			mensaje = "Es de mañana.";
+			break;
+		default:
+			mensaje = "No tengo la hora asignada";
+			break;
 	}
-	
+	alert(mensaje);
+
 }//FIN DE LA FUNCIÓN

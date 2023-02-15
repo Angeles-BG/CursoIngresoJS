d2106459ae7b73b8c704617c26afe7591ab96825
Angeles-BG <g.angeles112004@gmail.com>
2023-02-15 12:23:24 -0300

    modificación de ejercicios for

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -2,26 +2,32 @@
 Angeles Belen Garcia
 al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
 */
-function mostrar()
-{
-	let numero; 
-	let divisor; 
-	let mensaje; 
-	let cantidadDivisores; 
+function mostrar() {
+	let numero;
+	let divisor;
+	let mensaje;
+	let cantidadDivisores;
+
+	divisor = 0;
+	cantidadDivisores = 0;
 
 	numero = parseInt(prompt("Ingrese un numero: "));
 	while (isNaN(numero)) {
 		numero = parseInt(prompt("Error. Ingrese un numero: "));
 	}
 
-	for(let i=1; i <= numero; i++){
-		divisor = divisor + 1; 
-		if (numero % divisor == 0){
-			cantidadDivisores = cantidadDivisores +1 
-		}
-		if (cantidadDivisores <3 && i >3 ){
-			mensaje = "el numero es primo"; 
+	for (let i = 1; i <= numero; i++) {
+		divisor = divisor + 1;
+		if (numero % divisor == 0) {
+			cantidadDivisores = cantidadDivisores + 1;
 		}
 	}
-	alert(mensaje); 
+
+	if (cantidadDivisores < 3) {
+		mensaje = "el numero es primo";
+	} else {
+		mensaje = "no es primo ";
+	}
+
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN

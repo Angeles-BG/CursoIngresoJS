2df40bd2af966e3fc0b77d75701c85214502551b
Angeles-BG <g.angeles112004@gmail.com>
2023-02-16 10:07:25 -0300

    modificación de ejercicios while

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -14,10 +14,9 @@ con números DESCENDENTES, desde el 10 al 1.*/
 	}
 
 }//FIN DE LA FUNCIÓN
-*/
+
 //while 2 bis: Ingresar 5 números, y determinar 
 //la cantidad de números que son mayores que 10 y menores 20 (inclusive en ambos casos)
-
 function mostrar()
 {
 	let numero;
@@ -41,5 +40,46 @@ function mostrar()
 	}
 	alert("la cantidad de numeros mayores a diez es: "+ mayorDiez);
 	alert("la cantidad de numeros menores a 20 es: "+menorVeinte); 
+
+}//FIN DE LA FUNCIÓN
+*/
+//While 2 bis bis: Ingresar numeros hasta que el usuario quiera. 
+//Determinar la cantidad de números que son mayores que 10 y menores 20 
+//(inclusive en ambos casos). También el promedio de los números que cumplen la condición anterior.
+function mostrar()
+{
+	let numero;
+	let mayorDiez;
+	let menorVeinte; 
+	let promedio;
+	let ingresos; 
+	let respuesta; 
+	let sumaNumeros; 
+
+	mayorDiez = 0;
+	menorVeinte = 0; 
+	respuesta = "si"; 
+	sumaNumeros = 0;
+	ingresos = 0; 
+	
+	while (respuesta == "si") {
+		numero = parseInt(prompt("Ingrese un numero: "));
+		while (isNaN(numero)) {
+			numero= parseInt(prompt("Error. Ingrese un numero "));
+		}
+		if(numero>9){
+			mayorDiez = mayorDiez +1;
+			sumaNumeros = numero + sumaNumeros; 
+		}else if(numero<21){
+			menorVeinte = menorVeinte +1;
+			sumaNumeros = numero + sumaNumeros;
+		}
+		ingresos = ingresos +1; 
+		respuesta = prompt("¿Quiere ingresar otro numero? si/no"); 
+	}
+	promedio = sumaNumeros /ingresos;
+	alert("la cantidad de numeros mayores a diez es: "+ mayorDiez);
+	alert("la cantidad de numeros menores a 20 es: "+menorVeinte); 
+	alert("El promedio de los numeros que cumplen con la condicon de ser menor a 20 y/ o mayor a 10 es: " + promedio); 
 
 }//FIN DE LA FUNCIÓN
